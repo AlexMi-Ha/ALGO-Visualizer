faad7ca91123f45326ce5e8be11e025e3359401f
Alex Hagl <alex@hagl.de>
2023-11-23 10:08:33 +0100

    Added numbers

--- visualizer.js
+++ visualizer.js
@@ -8,9 +8,10 @@ class Element {
   
   class BarGraphVisualizer {
     
-    constructor(arr) {
+    constructor(arr, logContainer) {
       this.updateArr(arr);
       this.markedIndex = [];
+      this.logContainer = logContainer;
     }
 
     resetAll() {
@@ -31,10 +32,23 @@ class Element {
       for(let i = 0; i < this.arr.length; ++i) {
         drawRect(this.arr[i], i, widthScaleFactor, heightScaleFactor, this.markedIndex);
       }
+      drawIndex(0, this.arr.length, widthScaleFactor);
     }
     
     async delay(duration = speedSlider.value) {
       await sleep(duration);
+    }
+
+    log(text, level=0) {
+        if(level == 0) {
+            this.logContainer.log(text);
+        }else {
+            this.logContainer.logError(text);
+        }
+    }
+
+    clearLogs() {
+        this.logContainer.clear();
     }
     
     debug() {
@@ -83,14 +97,27 @@ class Element {
   const sleep = (delay) => new Promise(resolve => setTimeout(resolve, delay));
   
   function drawRect(element, index, widthScaleFactor, heightScaleFactor, markedIndex) {
+    const yOffset = 20;
+    noStroke();
+    textAlign(CENTER);
     if(element.selected) {
-      fill(255,0,0);
+      fill(255,0,0,128);
     }else if(element.finished) {
-      fill(0,255,0);
+      fill(0,200,0,128);
     }else if(markedIndex.includes(index)) {
-      fill(255, 255, 0);
+      fill(255, 255, 0,128);
     }else{
-      fill(0);
+      fill(0,0,0,128);
     }
-    rect(widthScaleFactor*index, height - element.value * heightScaleFactor, widthScaleFactor, element.value * heightScaleFactor);
+    rect(widthScaleFactor*index, height - element.value * heightScaleFactor - yOffset, widthScaleFactor, element.value * heightScaleFactor);
+    fill(255);
+    text(element.value, widthScaleFactor*index+widthScaleFactor/2, height-element.value*heightScaleFactor);
+  }
+
+  function drawIndex(min, max, widthScaleFactor) {
+    noStroke();
+    fill(0);
+    for(let i = min; i < max; ++i) {
+        text(i,i*widthScaleFactor + widthScaleFactor/2,height - 5);
+    }
   }
